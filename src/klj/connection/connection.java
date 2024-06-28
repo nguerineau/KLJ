ee89369152bea7e877e42c96bb79bc6313c1c0b7
nguerineau <62515842+nguerineau@users.noreply.github.com>
2024-06-28 10:16:51 +0100

    connection class creation
    super class for connection and identity

---
+++ src/klj/connection/connection.java
@@ -0,0 +1,18 @@
+package klj.connection;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author nicolasguerineau
+ */
+public class connection {
+    String username;
+    // password
+    int accestype; // admin or customer
+    // image de profil
+    
+}
