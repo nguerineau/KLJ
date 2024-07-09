8c949f62bbb7f9fac9b457b996e5532cee0f952a
nguerineau <62515842+nguerineau@users.noreply.github.com>
2024-07-09 16:50:03 +0100

    connecting to data
    version 1 non working

--- src/klj/KLJ.java
+++ src/klj/KLJ.java
@@ -3,6 +3,10 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package klj;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
 //Kid Land Java
 /**
  *
@@ -13,8 +17,13 @@ public class KLJ {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
+    public static void main(String[] args) throws SQLException {
         // TODO code application logic here
+        String username="tux";
+        String password="kantor";
+        String url="jdbc:mysql://localhost:8889/KLJ";
+        Connection con = DriverManager.getConnection(url,username,password);
+        
     }
     
 }
