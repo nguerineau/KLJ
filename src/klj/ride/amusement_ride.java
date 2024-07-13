7c3b26f65059980f1a905415407e215757ca9170
nguerineau <62515842+nguerineau@users.noreply.github.com>
2024-07-14 00:37:29 +0100

    Update amusement_ride.java

--- src/klj/ride/amusement_ride.java
+++ src/klj/ride/amusement_ride.java
@@ -4,11 +4,43 @@
  */
 package klj.ride;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import klj.display.register;
+
 /**
  *
  * @author nicolasguerineau
  */
 public class amusement_ride {
     // ticket adn ticket type
+    
+    
+    public amusement_ride(String ride_name){
+        int places=50;
+        String url="jdbc:mysql://localhost:8889/KLJ?zeroDateTimeBehavior=CONVERT_TO_NULL";
+        
+        Connection con;
+        try {
+            con = DriverManager.getConnection(url,"root","root");
+            String sql="SELECT password FROM account WHERE username="+"'"+"'";
+            
+            
+            Statement stmt=con.createStatement(); 
+            ResultSet rs=stmt.executeQuery(sql); 
+        
+            
+        }
+        catch (SQLException ex) {
+            Logger.getLogger(register.class.getName()).log(Level.SEVERE, null, ex);
+        }
+        
+        }
+    
     
 }
