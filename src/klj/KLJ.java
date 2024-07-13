2ace73b53271e762d88bd836f0013a6312962f00
nguerineau <62515842+nguerineau@users.noreply.github.com>
2024-07-13 15:38:04 +0100

    Update KLJ.java

--- src/klj/KLJ.java
+++ src/klj/KLJ.java
@@ -21,31 +21,7 @@ public class KLJ {
      */
     public static void main(String[] args) throws SQLException {
         // TODO code application logic here
-        String sql="SELECT * FROM account WHERE type=2";
-        String username ="root";
-        String password ="root";
         
-        
-        String url="jdbc:mysql://localhost:8889/KLJ?zeroDateTimeBehavior=CONVERT_TO_NULL";
-        Connection con = DriverManager.getConnection(url,username,password);
-        
-        Statement stmt=con.createStatement(); 
-        ResultSet rs=stmt.executeQuery(sql); 
-        
-        while(rs.next()){ 
-        System.out.println(rs.getString(1)+"  "+rs.getInt(2)); 
-        String test1 = rs.getString(1);
-        System.out.println(test1);
-        
-        }
-        
-        PreparedStatement stmt2 = con.prepareStatement("INSERT INTO account(username, type) VALUES (?, ?)");
-        stmt2.setString(1,"test5");
-        stmt2.setInt(2, 0);
-        stmt2.executeUpdate();
-        
-        //String rud="insert into account (username,type,password,email,date,country) \n" +"values \n" +"('test3',0,null,null,null,null);";
-        //stmt.executeQuery(rud); 
         
         
         
